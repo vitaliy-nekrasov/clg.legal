20364b697644f936d0f828fbb1a873392aa4d14f
Vitaliy Nekrasov <102180622+vitaliy-nekrasov@users.noreply.github.com>
2026-09-15 00:02:51 +0300

    Create abonent-ur-obs page

---
+++ assets/js/pages/service.js
@@ -0,0 +1,9 @@
+import { initCasesCta } from '../components/cases-cta.js';
+import { initTeam } from '../components/team.js';
+import { initFaq } from '../components/faq.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initCasesCta();
+  initTeam();
+  initFaq();
+});
